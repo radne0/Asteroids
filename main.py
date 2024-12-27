a014f7ce5af07f60e8f33e0d020da923155ca330
radne0 <jmcornelius0@gmail.com>
2024-12-27 04:05:04 -0500

    Game Loop updated:  frame limiting

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # initialize pygame.
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    # Framerate handling..
+    game_clock = pygame.time.Clock()
+    dt = 0
 
+
+    #Game Loop.
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill( "black"  )
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000                           # time between frame updates.  upper limit capped at 60 FPS  (no faster than ~16.7 ms between updates)
     
-
-
-
 
 
 # only excute when this file is ran directly, not imported.

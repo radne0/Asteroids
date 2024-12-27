11ebe3618bdb19c458b148b8333eecba4c17b0bc
radne0 <jmcornelius0@gmail.com>
2024-12-27 05:31:37 -0500

    Slight refactoring

--- player.py
+++ player.py
@@ -7,11 +7,19 @@ class Player(CircleShape):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
 
-# code from boot.dev
+# initial code from boot.dev
+# rewritten slightly
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+
+        #orthonormal basis.  unit vector in direction of ship  and one perpendicular to that.
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)                       #<-- unit vector in direction of ship
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90)                    #<-- perpendicular to ship
+
+
+        # front of ship placed on the circle away from the center in the direction the ship if facing
+        # back of ship vertices:  start by placing on the circle away from the center in the direction OPPOSITE from the direction of ship
+        # shift along the perpendicular "right" or "left" to a desired thickness.   
         a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        b = self.position - forward * self.radius - right*self.radius/1.5
+        c = self.position - forward * self.radius + right*self.radius/1.5
         return [a, b, c]

71d03ea1221db31c4af0fd9c2aa4c587fc1df5ad
radne0 <jmcornelius0@gmail.com>
2024-12-27 03:38:08 -0500

    main.py - initial game loop

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
-import constants
-
+from constants import * 
+from time import sleep
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    while(True):
+        screen.fill( (255,255,255)  )
+        pygame.display.flip()
+    
+
+
+
 
 
 # only excute when this file is ran directly, not imported.

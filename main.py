326e127e4f357eb28ca46e3c3a41c0a672f20fc9
radne0 <jmcornelius0@gmail.com>
2024-12-27 03:46:31 -0500

    Updated

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     while(True):
-        screen.fill( (255,255,255)  )
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill( "black"  )
         pygame.display.flip()
     
 

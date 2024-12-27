1cd46504ce1cae20624e10193e43dd3edbef13dd
radne0 <jmcornelius0@gmail.com>
2024-12-27 08:11:21 -0500

    main.py updated: asteroids spawn periodically random location and move in a random direction

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import * 
-from time import sleep
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -20,9 +21,14 @@ def main():
     #Game Loop.
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = updatable
 
     p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    asteroid_field = AsteroidField()
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:

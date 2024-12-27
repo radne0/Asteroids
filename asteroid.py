bacdb67bcd5510944b2e389a1616534aa80ac966
radne0 <jmcornelius0@gmail.com>
2024-12-27 08:08:35 -0500

    Implement asteroid random asteroid spawning / movement

--- asteroid.py
+++ asteroid.py
@@ -7,8 +7,9 @@ class Asteroid(CircleShape):
     def __init__(self,x,y,radius):
         super().__init__(x,y,radius)
 
+
     def draw(self,screen):
-        pygame.draw.circle(screen, (self.x,self.y),2)
+        pygame.draw.circle(screen, "white", (self.position.x,self.position.y),self.radius)
 
     def update(self,dt):
         self.position += self.velocity*dt
